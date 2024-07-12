5158444fae94b21a202f8ac0f2e9042bc0a48f30
Franck Darren <kikondanze@gmail.com>
2024-07-12 11:35:26 +0100

    ajout

--- app/etape-6/page.js
+++ app/etape-6/page.js
@@ -4,7 +4,7 @@ export default function Etape6() {
     return (
         <div className="flex min-h-screen flex-col items-center pt-[20px] bg-black">
             <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-white pt-[5px]">Arrêt 6</h1>
-            <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Terminez votre aventure parmi les fleurs en découvrant les nouveautés et exclusivités de MyCanal.Scannez le QR code à l&apos;entrée pour débuter votre aventure MyCanal et découvrir une introduction spéciale.</h2>
+            <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Scanner le QR Code à l&apos;entrée</h2>
             <div className="mx-auto mt-9">
                 <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
                     <rect width="600" height="600" style={{ fill: '#d8d2d2', strokeWidth: 3, stroke: 'white' }} />
--- app/indice-1/page.js
+++ app/indice-1/page.js
@@ -7,7 +7,7 @@ export default function Indice1() {
         <main className="flex min-h-screen flex-col items-center pt-[20px] bg-black">
             <div className="">
                 <h2 className="font-bold text-[30px] text-center text-white pt-[20px]">Indice pour l&apos;étape suivante</h2>
-                <p className="mt-10 px-5 text-[20px] text-center text-white">Pour le prochain indice, trouvez le lieu où vous pouvez regarder MyCanal en streaming sur votre mobile tout en admirant la vue tout en étant assis.
+                <p className="mt-10 px-5 text-[20px] text-center text-white">Pour le prochain indice, trouvez le lieu où vous pouvez regarder MyCanal en streaming sur votre mobile tout en admirant la vue en étant assis.
                 </p>
             </div>
             <Link href="/etape-2" className="text-white text-[20px] bg-[#d90000] hover:bg-[#571818] font-medium rounded-lg px-5 py-2.5  focus:outline-none mt-[40px]">Continuer</Link>
