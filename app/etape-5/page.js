1e1c42045b518c579b4dadf9d5befa64ca68d6bb
Franck Darren <kikondanze@gmail.com>
2024-07-12 09:18:38 +0100

    fix bug

--- app/etape-5/page.js
+++ app/etape-5/page.js
@@ -4,7 +4,7 @@ export default function Etape5() {
     return (
         <div className="flex min-h-screen flex-col items-center pt-[20px] bg-black">
             <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-white pt-[5px]">Etape 5</h1>
-            <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Bienvenue à la Baie des Rois ! Scannez le QR code à l'entrée pour débuter votre aventure MyCanal et découvrir une introduction spéciale.</h2>
+            <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Scannez le QR code à l&apos;entrée pour débuter votre aventure MyCanal et découvrir une introduction spéciale.</h2>
             <div className="mx-auto mt-9">
                 <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
                     <rect width="600" height="600" style={{ fill: '#d8d2d2', strokeWidth: 3, stroke: 'white' }} />
