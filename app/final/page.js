f688a17d042b25ed2fd6fd4be2999b8e4337c3c7
Franck Darren <kikondanze@gmail.com>
2024-07-12 11:19:01 +0100

    ajout

--- app/final/page.js
+++ app/final/page.js
@@ -1,13 +1,17 @@
 import Link from 'next/link';
 import Image from "next/image";
+import felicitation from '../../public/congratulations-7600_512.gif'
 
 export default function Final() {
     return (
-        <div className="flex min-h-screen flex-col items-center pt-[20px] bg-black">
-
+        <div className="flex min-h-screen flex-col items-center pt-[50px] bg-black">
             <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-white pt-[5px]">Félicitations</h1>
             <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Rendez-vous près du stand CANAL+ pour recevoir votre récompense. </h2>
-
+            <Image
+                src={felicitation}
+                alt="Description de l'image"
+                className="h-[400px] my-auto"
+            />
         </div>
 
     );
