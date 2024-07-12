efc0f2210d70f0342dfe06d3ef9085aef98692d2
Franck Darren <kikondanze@gmail.com>
2024-07-12 15:26:20 +0100

    add image

--- app/etape-2/page.js
+++ app/etape-2/page.js
@@ -1,4 +1,6 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from "next/image";
+import qrcode from '../../public/qrcode.jpeg'
 
 export default function Etape2() {
     return (
@@ -6,9 +8,11 @@ export default function Etape2() {
             <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-white pt-[5px]">Arrêt 2</h1>
             <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Depuis ce point de vue, imaginez regarder vos films et séries préférés sur MyCanal. </h2>
             <div className="mx-auto mt-9">
-                <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="600" height="600" style={{ fill: '#d8d2d2', strokeWidth: 3, stroke: 'white' }} />
-                </svg>
+                <Image
+                    src={qrcode}
+                    alt="Description de l'image"
+                    className="lg:h-[300px] lg:w-auto lg:mt-[20px]"
+                />
             </div>
             <Link href="/video-2" className="text-white text-[20px] bg-[#d90000] hover:bg-[#571818] font-medium rounded-lg px-5 py-2.5  focus:outline-none mt-[40px]">Continuer le défis</Link>
 
--- app/etape-3/page.js
+++ app/etape-3/page.js
@@ -1,4 +1,6 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from "next/image";
+import qrcode from '../../public/qrcode.jpeg'
 
 export default function Etape3() {
     return (
@@ -6,9 +8,11 @@ export default function Etape3() {
             <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-white pt-[5px]">Arrêt 3</h1>
             <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Imaginez profiter de vos repas en regardant des vidéos téléchargées sur MyCanal en présence du maître des lieux. <br />Demandez lui la spéciale MyCanal pour continuer votre aventure.</h2>
             <div className="mx-auto mt-9">
-                <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-                    <rect width="600" height="600" style={{ fill: '#d8d2d2', strokeWidth: 3, stroke: 'white' }} />
-                </svg>
+                <Image
+                    src={qrcode}
+                    alt="Description de l'image"
+                    className="lg:h-[300px] lg:w-auto lg:mt-[20px]"
+                />
             </div>
             <Link href="/video-3" className="text-white text-[20px] bg-[#d90000] hover:bg-[#571818] font-medium rounded-lg px-5 py-2.5  focus:outline-none mt-[40px] mb-[30px]">Continuer le défis</Link>
 
