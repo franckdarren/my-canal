e11c64af0c55cc4d63495cf7d164b8acf850c222
Franck Darren <kikondanze@gmail.com>
2024-07-18 21:53:20 +0100

    fix bug

--- app/final/page.js
+++ app/final/page.js
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 import Image from "next/image";
-import felicitation from '../../public/congratulations-7600_128.gif'
+import felicitation from '../../public/bravo.gif'
 
 export default function Final() {
     return (
-        <div className="flex min-h-screen flex-col items-center pt-[50px] lg:pt-[30px] bg-black">
-            <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-white pt-[5px]">Félicitations</h1>
-            <h2 className="font-bold text-[20px] text-center text-white pt-[20px] px-3">Rendez-vous près du stand CANAL+ pour recevoir votre récompense. </h2>
+        <div className="flex min-h-screen flex-col items-center pt-[50px] lg:pt-[30px] bg-white">
+            <h1 className="font-bold text-whitefont-bold text-[30px] text-center text-black pt-[5px]">Félicitations</h1>
+            <h2 className="font-bold text-[20px] text-center text-black pt-[20px] px-3">Rendez-vous près du stand CANAL+ pour recevoir votre récompense. </h2>
             <Image
                 src={felicitation}
                 alt="Description de l'image"
-                className="h-[400px] w-auto mt-[50px]"
+                className="h-[300px] w-auto"
             />
         </div>
 
